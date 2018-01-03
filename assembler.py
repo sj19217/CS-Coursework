aa3cd2315652656ae58b4447ba015072fcc0be83
sj9964 <stephenj99642@gmail.com>
2018-01-03 21:20:06 +0000

    Created a function to break the assembly into sections

--- assembler.py
+++ assembler.py
@@ -4,6 +4,8 @@ part work.
 """
 
 import re
+from pprint import pprint
+
 
 def normalise_text(text):
     # 1.1. Split text into lines
@@ -12,7 +14,7 @@ def normalise_text(text):
 
     for i, line in enumerate(lines):
         # 1.2. For each line, if there is a semicolon, remove everything after the first semicolon
-        lines[i] = lines[i].split("#")[0]
+        lines[i] = lines[i].split(";")[0]
 
         # 1.3. Strip all whitespace from the start and end of every line
         lines[i] = lines[i].strip()
@@ -30,6 +32,23 @@ def normalise_text(text):
 
     return normalised_text
 
+
+def split_into_sections(text):
+    # Don't bother with regular expressions for this
+    # Just split where "section." occurs
+    parts = text.split("section.")
+    parts_with_titles = [part.split("\n", maxsplit=1) for part in parts]
+
+    sections_dict = {}
+    for title, *other in parts_with_titles:
+        if title.strip() == "" and len(other) == 0:
+            continue
+
+        sections_dict[title] = other[0] if len(other) > 0 else ""
+
+    return sections_dict
+
+
 def main(asmfile):
     with open(asmfile, "rt") as file:
         text = file.read()
@@ -39,19 +58,21 @@ def main(asmfile):
     # 1. PERFORM TEXT NORMALISATION
 
     normalised_text = normalise_text(text)
-
     print(normalised_text)
 
-
-
-
-
-
+    # 2. SPLIT DOCUMENT INTO SECTIONS
+    section_dict = split_into_sections(normalised_text)
+    pprint(section_dict)
 
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) > 1:
-        main(sys.argv[1])
+        if sys.argv[1].lower() == "<ask>":
+            file = input("Input file: ")
+        else:
+            file = sys.argv[1]
+
+        main(file)
     else:
         print("Assembly file is unspecified")
---
+++ testing/for_section_split1.asm
@@ -0,0 +1,10 @@
+section.meta
+mem_amt 4
+name Hello
+
+section.data
+i VAR 0
+j VAR 1
+
+section.text
+; Commands here
