4d63da218306f7edfedd0373712599d4d90d3ab3
Stephen Jones <sj.19217@stocktonsfc.ac.uk>
2018-01-16 10:38:57 +0000

    Removed old version of .idea and Assembler

--- Assembler/testing/data_section.asm
+++
@@ -1,7 +0,0 @@
-section.meta
-
-section.data
-a VAR uchar 5
-b VAR int -100000
-
-section.text
--- Assembler/testing/for_normalisation1.asm
+++
@@ -1,6 +0,0 @@
-mov eax 5
-mov     ebx 7
-add eax            ebx
-
-mov eax [ecx]   ; Comment
-# Comment on its own line
