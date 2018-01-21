cd8bd644c49adce2b27d9c57e5da52813dd9a9e9
Stephen <stephenj99642@gmail.com>
2018-01-21 13:13:58 +0000

    Started the processing. Still some bugs but bytecode is now fully put into memory.

--- Interpreter/main.c
+++ Interpreter/main.c
@@ -16,33 +16,39 @@ struct {
     int memorykb;
 } config;
 
+struct DecodedInfo {
+    int dtype;
+    int op1_type;
+    int op2_type;
+};
+
 // Contains everything about the environment; registers, etc.
 struct {
     // The general registers. Smaller ones are contained within these.
-    long eax;
-    long ebx;
-    long ecx;
-    long edx;
-    long esi;
-    long edi;
-    long ebp;
-    long esp;
+    unsigned long eax;
+    unsigned long ebx;
+    unsigned long ecx;
+    unsigned long edx;
+    unsigned long esi;
+    unsigned long edi;
+    unsigned long ebp;
+    unsigned long esp;
 
     // The special registers
-    long pc;
-    long mar;
-    long mdr;
-    long cir;
+    unsigned long pc;
+    unsigned long mar;
+    unsigned long mdr;
+    unsigned long cir;
 
     // The memory
-    char memory[];
+    unsigned char* memory;
 } env;
 
 
 
 // FUNCTIONS
 
-void process_config(const char* bytecode, int length)
+int process_config(const unsigned char* bytecode, int length)
 {
     // Keep running until 4 nulls are reached
     int num_nulls = 0;
@@ -104,22 +110,183 @@ void process_config(const char* bytecode, int length)
     }
 
     // Print out the config struct
-    printf("config.memorykb = %i", config.memorykb);
+    printf("config.memorykb = %i\n", config.memorykb);
+
+    return strlen(config_str);
 }
 
-void run(const char* bytecode, int iflag, int length)
+int get_op_len(int type)
+{
+    switch (type) {
+        case 0: // None
+            return 0;
+        case 1: // Register
+            return 1;
+        case 2: // 8-bit immediate
+            return 1;
+        case 3: // 16-bit immediate
+            return 2;
+        case 4: // 32-bit immediate
+            return 4;
+        case 5: // Memory address
+            return 4;
+        case 6: // "a" form arithmetic
+            return 1;
+        case 7: // "a*b" form arithmetic
+            return 2;
+        case 8: // "a+b" form arithmetic
+            return 2;
+        case 9: // "a*b+c" form arithmetic
+            return 3;
+        case 10: // "a+b*c" form arithmetic
+            return 3;
+        default: // Invalid
+            return -1;
+    }
+}
+
+void run_loop()
+{
+    // Executes the instructions
+    while (1)
+    {
+        // Fetch
+        char opcode = env.memory[env.pc];
+
+        // If a HLT instruction, exit the loop
+        if (opcode == 0) {
+            break;
+        }
+
+        // Decode
+        char dtype = 'u';
+        int op1_type, op2_type;
+        if (opcode == 0x01 |
+                opcode == 0x20 | opcode == 0x28 |
+                opcode == 0x30 | opcode == 0x38 |
+                opcode == 0x40 | opcode == 0x48) {
+            dtype = 'b';
+        } else if (opcode == 0x02 |
+                opcode == 0x21 | opcode == 0x29 |
+                opcode == 0x31 | opcode == 0x39 |
+                opcode == 0x41 | opcode == 0x49) {
+            dtype = 'B';
+        } else if (opcode == 0x03 |
+                opcode == 0x22 | opcode == 0x2A |
+                opcode == 0x32 | opcode == 0x3A |
+                opcode == 0x42 | opcode == 0x4A) {
+            dtype = 'h';
+        } else if (opcode == 0x04 |
+                opcode == 0x23 | opcode == 0x2B |
+                opcode == 0x33 | opcode == 0x3B |
+                opcode == 0x43 | opcode == 0x4B) {
+            dtype = 'H';
+        } else if (opcode == 0x05 |
+                opcode == 0x24 | opcode == 0x2C |
+                opcode == 0x34 | opcode == 0x3C) {
+            dtype = 'i';
+        } else if (opcode == 0x06 |
+                opcode == 0x25 | opcode == 0x2D |
+                opcode == 0x36 | opcode == 0x3D |
+                opcode == 0x46 | opcode == 0x4D) {
+            dtype = 'I';
+        } else if (opcode == 0x07 |
+                opcode == 0x26 | opcode == 0x2E |
+                opcode == 0x36 | opcode == 0x3D |
+                opcode == 0x46 | opcode == 0x4D) {
+            dtype = 'f';
+        } else if ((opcode & 0b11110000) == 80 |
+                (opcode & 0b11110000) == 96) {
+            // One of the size-based commands
+            if ((opcode & 0b00001111) == 0) {
+                dtype = '1';
+            } else if ((opcode & 0b00001111) == 1) {
+                dtype = '2';
+            } else if ((opcode & 0b00001111) == 2) {
+                dtype = '4';
+            }
+        } else if (opcode == 0x10) {
+            dtype = '1';
+        } else if (opcode == 0x11) {
+            dtype = '2';
+        } else if (opcode == 0x12) {
+            dtype = '4';
+        } else if ((opcode > 0x08) & (opcode < 0x0E)) {
+            dtype = 'n';
+        } else if (opcode == 0x14) {
+            dtype = 'n';
+        }
+
+        // Interpret the operand type
+        op1_type = (env.memory[env.pc+1] & 0b11110000) >> 4;
+        op2_type = env.memory[env.pc+1] & 0b00001111;
+
+        if (get_op_len(op1_type) == -1 | get_op_len(op2_type) == -1) {
+            // One of them is invalid
+            printf("Invalid operand byte 0x%x at PC %lu", env.memory[env.pc+1], env.pc+1);
+        }
+
+        // Pull the first operand as a char[]
+        int op1_len = get_op_len(op1_type);
+        int op2_len = get_op_len(op2_type);
+        unsigned char op1_str[op1_len];
+        unsigned char op2_str[op2_len];
+
+        // Move the PC along to the start of the first operand and move to op1_str
+        env.pc += 2;
+        for (int i = 0; i < op1_len; i++) {
+            op1_str[i] = env.memory[env.pc + i];
+        }
+
+        // Move the PC along again and interpret
+        env.pc += op1_len;
+        for (int i = 0; i < op2_len; i++) {
+            op2_str[i] = env.memory[env.pc + i];
+        }
+
+        // Move the PC past the second operand (should be to the next opcode byte)
+        env.pc += op2_len;
+
+        // Print out info
+        printf("opcode=0x%x, dtype=%c, op1_type=%i, op2_type=%i\n", opcode, dtype, op1_type, op2_type);
+        printf("\tOperand 1: ");
+        for (int i = 0; i < op1_len; i++) {
+            printf("%x ", op1_str[i]);
+        }
+        printf("\n\tOperand 2: ");
+        for (int i = 0; i < op2_len; i++) {
+            printf("%x ", op2_str[i]);
+        }
+        printf("\n");
+    }
+}
+
+void run(unsigned char* bytecode, int iflag, int length)
 {
     printf("Executing run()\n");
 
+    int bi;
+    for (bi = 0; bi < length; bi++) {
+        printf("%x ", bytecode[bi]);
+    }
+
     // Begin the process of running the bytecode
     // Start by loading all of the configuration data
-    process_config(bytecode, length);
+    int config_length = process_config(bytecode, length) + 4;
 
     // Initialise the environment
-    char memory[config.memorykb*1024];
-    env.memory = memory;
+    const int memsize = config.memorykb * 1024;
+    env.memory = (unsigned char*) malloc(memsize*sizeof(unsigned char));
 
-    //
+    // Fill up the memory with the instructions
+    for (int i = config_length; i < length; i++) {
+        env.memory[i - config_length] = bytecode[i];
+        printf("0x%x: %x\n", i - config_length, env.memory[i - config_length]);
+    }
+
+
+    // Start running instructions
+    run_loop();
 }
 
 int main(int argc, char** argv)
@@ -128,6 +295,9 @@ int main(int argc, char** argv)
     int iflag = 0;
     char fname[100];
     int has_fname = 0;
+
+    // -i means instructions, meaning that the GUI program is listening and wants display instructions
+    // -f <filename>, meaning the filename
 
     int c;
     while ((c = getopt(argc, argv, "if:")) != -1)
@@ -153,7 +323,7 @@ int main(int argc, char** argv)
 
     // Read the file
     FILE* fileptr;
-    char* content;
+    unsigned char* content;
     long filelen;
 
     fileptr = fopen(fname, "rb");
@@ -168,7 +338,7 @@ int main(int argc, char** argv)
     rewind(fileptr);
 
 
-    content = (char*)malloc(filelen*sizeof(char));
+    content = (unsigned char*)malloc(filelen*sizeof(char));
     fread(content, (size_t) filelen, 1, fileptr);
     fclose(fileptr);
 
--- Interpreter/process.md
+++ Interpreter/process.md
@@ -9,3 +9,47 @@
 * Looked up how to interpret an int from the value string (http://en.cppreference.com/w/c/string/byte/strtol)
 * Looked up how to compare strings (https://www.tutorialspoint.com/c_standard_library/c_function_strcmp.htm)
 * Wrote code to add config info into struct
+* Initialise the environment; had some trouble with how to create a char array correctly
+* Wrote run_loop function (with a large if statement). Programmed a while 1 loop and get it to process opcode
+  and operand byte, but not yet get the operands or move along (this was an oversight)
+* Found that the memory was not correctly made. Old code was:
+
+```
+int passed_delim = 0;
+    int num_nulls = 0;
+    int length_pre_instr = 0;
+    for (int i = 0; i < length; i++) {
+        if (passed_delim == 0) {
+            if (bytecode[i] == 0) {
+                num_nulls++;
+            }
+
+            if (num_nulls == 4) {
+                passed_delim = 1;
+                length_pre_instr = i;
+            }
+        } else {
+            env.memory[i - length_pre_instr] = bytecode[i];
+        }
+    }
+```
+
+* Fiddled around and used debugger, print hex version of bytecode
+* Replaced code with:
+
+```
+for (int i = config_length; i < length - config_length; i++) {
+        env.memory[i - config_length] = bytecode[i];
+    }
+```
+
+* That seemed to work, but run_loop stayed on char 1 and didn't iterate
+* Added ability to consume operands into a char[]; created get_op_len
+* Wrote code and wrote text to output data about each opcode and operand (printing raw operand hex)
+* Got a very strange error where everything printed fine until memory address 51, when suddenly it broke and contained
+  bytes not present in the original bytecode
+* After much messing around and using debugger, determined it was probably the memory not being transferred right
+* Changed memory from being a new array to being based on malloc
+* Added memory dump to the loop and saw that the memory stops at address 49, meaning not all of the bytecode is placed
+  into the memory
+* Changed i<length-config_length to i<length, because this was cutting it off short
