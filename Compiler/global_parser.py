96d39051de1021001f9741ea2632442ab2fb1b3a
Stephen <stephenj99642@gmail.com>
2018-02-07 18:59:33 +0000

    Global symbol table holds functions

--- Compiler/global_parser.py
+++ Compiler/global_parser.py
@@ -1,9 +1,10 @@
 import logging
 from collections import namedtuple
 
-from pycparser.c_ast import Decl
+from pycparser.c_ast import Decl, FuncDef
 
-GlobalVariable = namedtuple("GlobalVariable", "name type")
+GlobalVariable = namedtuple("GlobalVariable", "name type initial")
+GlobalFunction = namedtuple("GlobalFunction", "name definition")
 
 def global_parser(tree):
     table = []
@@ -12,6 +13,7 @@ def global_parser(tree):
         if isinstance(top_node, Decl):
             name = top_node.name
             type_list = top_node.type.type.names
+            initial = top_node.init
 
             if "char" in type_list:
                 str_type = "char"
@@ -26,6 +28,12 @@ def global_parser(tree):
             if "unsigned" in type_list:
                 str_type = "u" + str_type
 
-            logging.info("Found global variable {name} of type {type}".format(name=name, type=str_type))
+            logging.info("Found global variable {name} of type {type}, initial {init}".format(name=name, type=str_type, init=initial))
 
-            table.append(GlobalVariable(name, str_type))
+            table.append(GlobalVariable(name, str_type, initial))
+        elif isinstance(top_node, FuncDef):
+            if top_node.decl.name == "main":
+                table.append(GlobalFunction("main", top_node))
+                logging.info("Found global function main")
+            else:
+                logging.warning("Found global function that was not main; this is not yet supported")
