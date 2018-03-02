fe225d9b95b071dcda3ea73c279f76384052f302
Stephen <stephenj99642@gmail.com>
2018-03-02 17:16:53 +0000

    Added comments

--- Compiler/testing/outputs/while.asm
+++ Compiler/testing/outputs/while.asm
@@ -9,58 +9,78 @@ pow VAR int 1
 section.text
 MOV 4B esp 2048
 MOV 4B ebp 2048
-while_3c030e41 SUB uint esp 4
+; Beginning while loop
+while_5a227c2e ; Pushing global variable to stack
+SUB uint esp 4
 MOV 4B [esp] count
+; Pushing constant to stack
 SUB uint esp 4
 MOV 4B [esp] 5
+; Evaluating binary expression: Popping values to registers.
 MOV 4B edx [esp]
 ADD uint esp 4
 MOV 4B ecx [esp]
 ADD uint esp 4
+; Making comparison (<)
 CMP uint ecx edx
-JLT jmptrue_0d21f69d
-JMP jmpfalse_0d21f69d
-jmptrue_0d21f69d SUB esp 4
+JLT jmptrue_a9b95a09
+JMP jmpfalse_a9b95a09
+jmptrue_a9b95a09 SUB esp 4
 MOV 4B [esp] 1
-JMP jmpcmpend_0d21f69d
-jmpfalse_0d21f69d SUB esp 4
+JMP jmpcmpend_a9b95a09
+jmpfalse_a9b95a09 SUB esp 4
 MOV 4B [esp] 0
-jmpcmpend_0d21f69d MOV 4B eax eax
-CMP int [esp] 1
-JNE endwhile_3c030e41
+jmpcmpend_a9b95a09 MOV 4B eax eax    ; Determined truth and added to stack
+CMP int [esp] 1  ; See if true and jump accordingly
+JNE endwhile_5a227c2e
+; Running child block
+; Pushing global variable to stack
 SUB uint esp 4
 MOV 4B [esp] count
+; Pushing constant to stack
 SUB uint esp 4
 MOV 4B [esp] 1
+; Evaluating binary expression: Popping values to registers.
 MOV 4B edx [esp]
 ADD uint esp 4
 MOV 4B ecx [esp]
 ADD uint esp 4
+; Performing + and pushing to stack
 ADD int ecx edx
 SUB uint esp 4
 MOV 4B [esp] ecx
-LEA edi count
+; Assigning top of stack to variable count
+LEA edi count      ; Pointer to a global
+; Move from stack to variable
 MOV 4B ecx [esp]
 ADD uint esp 4
 MOV 4B [edi] ecx
+; Pushing global variable to stack
 SUB uint esp 4
 MOV 4B [esp] pow
+; Pushing constant to stack
 SUB uint esp 4
 MOV 4B [esp] 2
+; Evaluating binary expression: Popping values to registers.
 MOV 4B edx [esp]
 ADD uint esp 4
 MOV 4B ecx [esp]
 ADD uint esp 4
+; Performing * and pushing to stack
 MUL int ecx edx
 SUB uint esp 4
 MOV 4B [esp] ecx
-LEA edi pow
+; Assigning top of stack to variable pow
+LEA edi pow      ; Pointer to a global
+; Move from stack to variable
 MOV 4B ecx [esp]
 ADD uint esp 4
 MOV 4B [edi] ecx
+; Pushing global variable to stack
 SUB uint esp 4
 MOV 4B [esp] pow
+; Calling printf
 MOV 4B out [esp]
 ADD uint esp 4
-endwhile_3c030e41 MOV 4B eax eax
+endwhile_5a227c2e MOV 4B eax eax
 exit HLT
