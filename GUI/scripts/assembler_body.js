57c54a68f2ec18f922d710d1937ecf2b682acfff
Stephen <stephenj99642@gmail.com>
2018-03-03 22:41:08 +0000

    Created basic file choosing for assembler

---
+++ GUI/scripts/assembler_body.js
@@ -0,0 +1,17 @@
+$ = require("jquery");
+remote = require("electron").remote;
+dialog = remote.dialog;
+
+$("#choose-file").on("click", () => {
+    console.log("File opener button clicked");
+    console.log(dialog.showOpenDialog((fileNames) => {
+        if (fileNames === undefined) {
+            console.log("No file given");
+            return;
+        }
+
+        console.log("File name: " + fileNames[0]);
+
+        window.location.href = "assembler_active.html?fname=" + encodeURIComponent(fileNames[0]);
+    }));
+});
